45aa9811c343f1c7a67f15a0ac4214b6d3ad480c
Taha Meskini <taha.meskini@ominimo.eu>
2026-07-01 12:30:32 +0200

    feat: add Gate policy guessing to ServiceProvider

--- src/CrudifyServiceProvider.php
+++ src/CrudifyServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Taha\Crudify;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Taha\Crudify\Policies\CrudifyPolicy;
 
 class CrudifyServiceProvider extends ServiceProvider
 {
@@ -11,6 +13,16 @@ class CrudifyServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/crudify.php' => config_path('crudify.php'),
         ], 'crudify-config');
+
+        Gate::guessPolicyNamesUsing(function (string $modelClass) {
+            $policyClass = str_replace('Models', 'Policies', $modelClass) . 'Policy';
+
+            if (class_exists($policyClass)) {
+                return $policyClass;
+            }
+
+            return CrudifyPolicy::class;
+        });
     }
 
     public function register(): void

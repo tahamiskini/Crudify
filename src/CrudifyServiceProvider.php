db269176900cb24b217fe3cf291636a589ecb169
Taha Meskini <taha.meskini@ominimo.eu>
2026-07-09 19:35:10 +0200

    feat : Route macro + ServiceProvider

--- src/CrudifyServiceProvider.php
+++ src/CrudifyServiceProvider.php
@@ -3,7 +3,9 @@
 namespace Taha\Crudify;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Taha\Crudify\Commands\GenerateRoutesCommand;
 use Taha\Crudify\Policies\CrudifyPolicy;
 
 class CrudifyServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class CrudifyServiceProvider extends ServiceProvider
 
             return CrudifyPolicy::class;
         });
+
+        $this->registerRouteMacro();
     }
 
     public function register(): void
@@ -31,5 +35,48 @@ class CrudifyServiceProvider extends ServiceProvider
             __DIR__ . '/../config/crudify.php',
             'crudify'
         );
+
+        $this->commands([
+            GenerateRoutesCommand::class,
+        ]);
+    }
+
+    protected function registerRouteMacro(): void
+    {
+        Route::macro('crud', function (string $resource, string $model, array $options = []) {
+            $controller = CrudifyController::class;
+            $namespace = $options['namespace'] ?? config('crudify.namespace', 'App');
+            $modelName = class_basename($model);
+
+            $def = function (array|string $method, string $action, string $suffix = '') use ($controller, $modelName, $namespace, $resource) {
+                $uri = $resource . $suffix;
+
+                $route = Route::match((array) $method, $uri, [$controller, $action]);
+
+                $route->setAction(array_merge(
+                    $route->getAction(),
+                    ['model' => $modelName, 'namespace' => $namespace]
+                ));
+            };
+
+            // Mass utility routes (before {id} routes to avoid capture)
+            $def('POST', 'massCreate', '/mass-create');
+            $def(['PUT', 'PATCH'], 'massUpdate', '/mass-update');
+            $def('DELETE', 'massDelete', '/mass-delete');
+            $def('POST', 'massCreateOrUpdate', '/mass-create-or-update');
+
+            // Single-resource routes
+            $def('GET', 'readMore');
+            $def('GET', 'readOne', '/{id}');
+            $def('POST', 'create');
+            $def(['PUT', 'PATCH'], 'update', '/{id}');
+            $def('DELETE', 'delete', '/{id}');
+
+            // Relation routes
+            $def('POST', 'addRelation', '/{id}/add-relation/{relation}');
+            $def('DELETE', 'removeRelation', '/{id}/remove-relation/{relation}/{relationId?}');
+            $def('POST', 'attachRelation', '/{id}/attach-relation/{relation}/{relationId}');
+            $def('DELETE', 'detachRelation', '/{id}/detach-relation/{relation}/{relationId}');
+        });
     }
 }
